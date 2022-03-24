083e0075b6bd4644e3184e25da6626f46cc128a8
shintaka87 <86876454+shintaka87@users.noreply.github.com>
2022-03-24 23:03:36 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
 import "./styles.css";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -13,10 +13,12 @@ export default function App() {
         <Link to="/page1">Page1</Link>
         <br />
         <Link to="/page2">Page2</Link>
-        <Home />
-        <Page1 />
-        <Page2 />
       </div>
+      <Switch>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
